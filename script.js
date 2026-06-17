56df5673542b579e32f2ff376e83e79ae5acc724
Marko Bakić <e.marko@telekom.rs>
2026-06-17 13:58:29 +0200

    url

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 (function () {
     'use strict';
 
-    const WEBHOOK_URL = "https://sinisa1989.app.n8n.cloud/webhook-test/telekom-complaint"
+    // const WEBHOOK_URL = "https://sinisa1989.app.n8n.cloud/webhook-test/telekom-complaint"
+    const WEBHOOK_URL = "https://sinisa1989.app.n8n.cloud/webhook/telekom-complaint"
 
     /* --- DOM refs ------------------------------------------ */
     const form          = document.getElementById('complaintForm');
